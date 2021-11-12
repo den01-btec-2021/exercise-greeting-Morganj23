08de2666d5cc4891d23920704e9be676f6349fbd
Morgan Jones <21074@bridgend.ac.uk>
2021-11-12 14:03:52 +0000

    complete

--- src/exercise.py
+++ src/exercise.py
@@ -2,6 +2,6 @@ def main():
     #write your code below this line
     print("what is your name")
     name=str(input(""))
-    print("hi "+ name)
+    print("Hi "+ name)
 if __name__ == '__main__':
     main()
